9a2802b50537f11154829e648ed7c039dc36f4c9
bojorquezja <bojorquez.ja@gmail.com>
2025-06-13 19:47:11 -0500

    sudoku resuelto

--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,12 @@
-tablero=[[1,1,1,1,0,1,1,1,1],
-     [1,0,0,1,0,1,0,0,0],
-     [1,1,0,1,1,1,1,0,1],
-     [0,1,0,1,0,0,1,0,1],
-     [1,1,1,1,1,1,1,1,1],
-     [1,0,1,0,0,0,1,0,1],
-     [1,1,1,1,0,1,1,0,1],
-     [1,0,0,1,0,1,0,0,1],
-     [1,1,1,1,0,1,1,1,1]]
+tablero=[[0, 6, 0, 1, 0, 4, 0, 5, 0],
+    [0, 0, 8, 3, 0, 5, 6, 0, 0],
+    [2, 0, 0, 0, 0, 0, 0, 0, 1],
+    [8, 0, 0, 4, 0, 7, 0, 0, 6],
+    [0, 0, 6, 0, 0, 0, 3, 0, 0],
+    [7, 0, 0, 9, 0, 1, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 0, 7, 2, 0, 6, 9, 0, 0],
+    [0, 4, 0, 5, 0, 8, 0, 7, 0]]
 
 
 def imprime(mat):
@@ -18,65 +18,46 @@ def imprime(mat):
 
 
 
-def valida(tablero, fil, col)->bool:
-    #verificar que no se repita nro en las filas
-    #verificar que no se repita nro en las columnas
-    #verificar que no se repita nro en el mini cuadrado
-    if fil >= len(lab):      
+def es_valido(tablero, fila, col, num):
+    if num in tablero[fila]:
         return False
-    if fil < 0:
-        return False
-    if col >= len(lab[0]):
-        return False
-    if col < 0:
-        return False
-    if tablero[fil][col] == 0:             
-        return False
-    if tablero[fil][col] == 1:
-        return False
+
+    for i in range(len(tablero)):
+        if tablero[i][col] == num:
+            return False
+
+    inicio_fila = (fila // 3) * 3
+    inicio_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if tablero[inicio_fila + i][inicio_col + j] == num:
+                return False
 
     return True
 
-def estalleno():
-    for
-        for
-    return fil, col
-    
-def sudoku(tablero, fil, col, num)->bool:
-    fil,col = estalleno()
-    if fil == -1 and col == -1:
-        #ok
-        return True
-
-    else:
-
-        if valida(tablero, fil, col):
-            tablero[fil][col]=num
-            imprime(tablero)
-
-            if labbas(tablero, fil+1, col, num):
-                return True
-            elif labbas(tablero, fil, col+1, num):
-                return True
-            elif labbas(tablero, fil, col-1, num):
-                return True
-            elif labbas(tablero, fil-1, col, num):
-                return True
-            else:
-                tablero[fil][col]=0
+def resolver_sudoku(tablero):
+    #recorro cada celda del laberinto buscando las disponibles = 0
+    for fila in range(len(tablero)):
+        for col in range(len(tablero[0])):
+            if tablero[fila][col] == 0:
+                #si encuentro disponible le pongo valor (1-9)
+                for num in range(1, 10):
+                    if es_valido(tablero, fila, col, num):
+                        tablero[fila][col] = num
+                        if resolver_sudoku(tablero):
+                            return True
+                        tablero[fila][col] = 0
                 return False
-
-        else:
-
-            return False
+    return True
 
 ######
 
 #imprime(lab)
 
 
-
-if sudoku(lab, 0, 0, 0):
+imprime(tablero)
+if resolver_sudoku(tablero):
     print("Salio")
+    imprime(tablero)
 else:
     print("No hay salida")
